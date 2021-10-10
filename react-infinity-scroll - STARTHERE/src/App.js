7304130c9aec8e7f3efdc1b15d6450f033c156b1
Matt Jia <mjia@redhat.com>
2021-10-10 19:48:03 +1000

    done work

--- react-infinity-scroll - STARTHERE/src/App.js
+++ react-infinity-scroll - STARTHERE/src/App.js
@@ -1,24 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react"
 // Components
-import User from './User';
+import User from "./User"
 // Styles
-import { Content, Loading } from './App.styles';
+import { Content, Loading } from "./App.styles"
 // API
-import { getUsers } from './API';
+import { getUsers } from "./API"
 
 function App() {
-  const [page, setPage] = useState(1);
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1)
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true)
 
-  return (
-    <div className='App'>
-      <Content>
-        {users && users.map(user => <User key={user.cell} user={user} />)}
-      </Content>
-      {loading && <Loading>Loading ...</Loading>}
-    </div>
-  );
+    const handleScroll = (event) => {
+        const { scrollTop, clientHeight, scrollHeight } = event.currentTarget
+        if (scrollHeight - scrollTop == clientHeight) {
+            setPage((prev) => prev + 1)
+        }
+    }
+
+    useEffect(() => {
+        const loadUsers = async () => {
+            setLoading(true)
+            const newUsers = await getUsers(page)
+            setUsers((prev) => [...prev, ...newUsers])
+            setLoading(false)
+        }
+        loadUsers()
+    }, [page])
+
+    return (
+        <div className="App">
+            {users && (
+                <Content onScroll={handleScroll}>
+                    {users.map((user) => (
+                        <User key={user.cell} user={user} />
+                    ))}
+                </Content>
+            )}
+            {loading && <Loading>Loading ...</Loading>}
+        </div>
+    )
 }
 
-export default App;
+export default App
--- react-infinity-scroll - STARTHERE/src/App.styles.js
+++ react-infinity-scroll - STARTHERE/src/App.styles.js
@@ -1,16 +1,16 @@
-import styled from 'styled-components';
+import styled from "styled-components"
 
 export const Content = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  width: 800px;
-  height: 800px;
-  margin: 0 auto;
-  overflow: auto;
-`;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    width: 800px;
+    height: 800px;
+    margin: 0 auto;
+    overflow: auto;
+`
 
 export const Loading = styled.div`
-  width: 200px;
-  margin: 20px auto;
-  text-align: center;
-`;
+    width: 200px;
+    margin: 20px auto;
+    text-align: center;
+`
